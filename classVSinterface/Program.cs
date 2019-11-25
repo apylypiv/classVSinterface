bf1e0d0b055b1a648a114faa291402ad3e60cdb2
apylypiv <apylypiv@gmail.com>
2019-11-25 18:56:28 +0200

    First commit

---
+++ classVSinterface/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classVSinterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("indexFrom = ");
+            //int indexFrom = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("indexTo = ");
+            //int indexTo = int.Parse(Console.ReadLine());
+
+            listClass callList = new listClass();
+            callList.listInit(5,8);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ classVSinterface/listClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classVSinterface
+{
+    class listClass
+    {
+        private int indexFrom;
+        public int _indexFrom
+        {
+            get
+            {
+                return indexFrom;
+            }
+            set
+            {
+                indexFrom = value;
+            }
+        }
+
+        private int indexTo;
+        public int _indexTo
+        { 
+            get
+            {
+                return indexTo;
+            }
+            set
+            {
+                indexTo = value;
+            }
+        }
+
+        public IList<int> testList = new List<int>();
+
+        //public void listInit(IList<int> testList)
+        public void listInit(int indexFrom, int indexTo)
+        {
+            IList<int> returnList = new List<int>();
+
+            testList.Add(0);
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+            testList.Add(6);
+            testList.Add(7);
+            testList.Add(8);
+            testList.Add(9);
+
+
+            for (int index = indexFrom; index <= indexTo; index++)
+            {
+                //returnList.Add(testList);
+                //Console.WriteLine(index);
+                Console.WriteLine(testList[index]);
+            }
+            //var t = testList[8];
+            //Console.WriteLine(t);
+
+            //string stringList = returnList.ToString();
+            //Console.WriteLine(stringList);
+
+
+            return;
+        }
+
+
+    }
+}
